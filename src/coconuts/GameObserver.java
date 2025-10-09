5f47524b088432c878494ace780c21c1bac4a82c
higaredajra <higaredajra@msoe.edu>
2025-10-09 11:26:18 -0500

    Create observer and subject interfaces

---
+++ src/coconuts/GameObserver.java
@@ -0,0 +1,19 @@
+/*
+ * Course: SWE2410-121
+ * Fall 2025-2026
+ * File header contains class GameObserver
+ * Name: higaredajra
+ * Created 10/9/2025
+ */
+package coconuts;
+
+/**
+ * Course SWE2410-121
+ * Fall 2025-2026
+ * Interface GameObserver Purpose: Observer interface to track hits of coconuts, the crab, and the beach
+ *
+ * @author higaredajra
+ * @version created on 10/9/2025 11:23 AM
+ */
+public interface GameObserver {
+}
---
+++ src/coconuts/Subject.java
@@ -0,0 +1,19 @@
+/*
+ * Course: SWE2410-121
+ * Fall 2025-2026
+ * File header contains class Subject
+ * Name: higaredajra
+ * Created 10/9/2025
+ */
+package coconuts;
+
+/**
+ * Course SWE2410-121
+ * Fall 2025-2026
+ * Interface Subject Purpose: Subject interface for a game object being observed
+ *
+ * @author higaredajra
+ * @version created on 10/9/2025 11:25 AM
+ */
+public interface Subject {
+}
