e20e10b7ac7a110897dce83b03e22dad867f5102
john eckberg <john.c.eckberg@gmail.com>
2025-10-09 11:32:46 -0500

    Added ReadMe.md, Added ScoreBoard.java

---
+++ ReadMe.md
@@ -0,0 +1,8 @@
+# Coconut Observer Lab
+
+Crab and Coconut observers: Crab and coconuts are being observed, 
+crab and coconut must extend the subject class. When the coconuts hit the ground or 
+get shot, we notify the concrete observer, in this case the scoreboard object, 
+and another observer that removes objects
+
+The scoreboard needs to encapsulate the javaFX components that make up the GUI 
---
+++ src/coconuts/ScoreBoard.java
@@ -0,0 +1,28 @@
+/*
+ * Course: SWE2410-121
+ * Fall 2025-2026
+ * File header contains class ScoreBoard
+ * Name: eckbergj
+ * Created 10/9/2025
+ */
+package coconuts;
+
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ * Course SWE2410-121
+ * Fall 2025-2026
+ * Class ScoreBoard Purpose: implements the scoreboard; implements the observer interface
+ *
+ * @author eckbergj
+ * @version created on 10/9/2025 11:29 AM
+ */
+public class ScoreBoard implements Observer {
+
+    @Override
+    public void update(Observable o, Object arg) {
+
+    }
+}
+    
