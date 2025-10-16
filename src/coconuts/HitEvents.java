b9fbd4088d057fe35e2be3c3d29e9a8a6d136e52
higaredajra <higaredajra@msoe.edu>
2025-10-16 11:24:30 -0500

    Create HitEvents enum

---
+++ src/coconuts/HitEvents.java
@@ -0,0 +1,23 @@
+/*
+ * Course: SWE2410-121
+ * Fall 2025-2026
+ * File header contains enum HitEvents
+ * Name: higaredajra
+ * Created 10/16/2025
+ */
+package coconuts;
+
+/**
+ * Course SWE2410-121
+ * Fall 2025-2026
+ * Enum HitEvents Purpose: Various kinds of hit events that can occur
+ *
+ * @author higaredajra
+ * @version created on 10/16/2025 11:16 AM
+ */
+public enum HitEvents {
+    LASER,
+    CRAB,
+    BEACH,
+    NULL_EVENT
+}
